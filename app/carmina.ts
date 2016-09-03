d57d3b2018a4642bc277bfbee14476a2ae538883
Ethan T Schmidt <etschmidt@users.noreply.github.com>
2016-09-02 17:35:03 -0700

    HTML Strings
    Added HTML input into the text strings

--- app/carmina.ts
+++ app/carmina.ts
@@ -4,15 +4,15 @@ export const CARMINA: Carmen[] = [
 	{ 
 		id: 1,
 		caput: 'I. ad Corneliam',
-		latin: `Cui dono lepidum novum libellum
-						arida modo pumice expolitum?
-						Corneli, tibi: namque tu solebas
-						meas esse aliquid putare nugas.
-						Iam tum, cum ausus es unus Italorum
-						omne aevum tribus explicare cartis . . .
-						Doctis, Iuppiter, et laboriosis!
-						Quare habe tibi quidquid hoc libelli—
-						qualecumque, quod, o patrona virgo,
+		latin: `Cui dono lepidum novum libellum<br>
+						arida modo pumice expolitum?<br>
+						Corneli, tibi: namque tu solebas<br>
+						meas esse aliquid putare nugas.<br>
+						Iam tum, cum ausus es unus Italorum<br>
+						omne aevum tribus explicare cartis . . .<br>
+						Doctis, Iuppiter, et laboriosis!<br>
+						Quare habe tibi quidquid hoc libelli—<br>
+						qualecumque, quod, o patrona virgo,<br>
 						plus uno maneat perenne saeclo!`,
 		english: `To whom do I give this pretty little book`
 	},
@@ -39,5 +39,41 @@ export const CARMINA: Carmen[] = [
 		caput: 'V. ad Lesbiam',
 		latin: `Vivamus mea Lesbia, atque amemus,`,
 		english: `Let us live, my Lesbia, and love,`
-	}
+	},
+	{ 
+		id: 6,
+		caput: 'VI. ad Flavium',
+		latin: `Flavi, delicias tuas Catullo,`,
+		english: `Let us live, my Lesbia, and love,`
+	},	
+	{ 
+		id: 7,
+		caput: 'V. ad Lesbiam',
+		latin: `Quaeris, quot mihi basiationes`,
+		english: `Let us live, my Lesbia, and love,`
+	},
+	{ 
+		id: 8,
+		caput: 'VIII. ad se ipsum',
+		latin: `Miser Catulle, desinas ineptire,`,
+		english: `Let us live, my Lesbia, and love,`
+	},
+	{ 
+		id: 9,
+		caput: 'IX. ad Veranium',
+		latin: `Verani, omnibus e meis amicis`,
+		english: `Let us live, my Lesbia, and love,`
+	},
+	{ 
+		id: 10,
+		caput: 'X. ad Varum',
+		latin: `Varus me meus ad suos amores`,
+		english: `Let us live, my Lesbia, and love,`
+	},
+	{ 
+		id: 11,
+		caput: 'XI. ad Furium et Aurelium',
+		latin: `Furi et Aureli comites Catulli,`,
+		english: `Let us live, my Lesbia, and love,`
+	},					
 ]
