f18d6c1075107476128245a35507c5ca31d1222b
Ethan T Schmidt <etschmidt@users.noreply.github.com>
2016-09-05 20:28:02 -0700

    meter
    added meter and base html files

--- app/carmen.ts
+++ app/carmen.ts
@@ -1,6 +1,7 @@
 export class Carmen {
 	id: number;
 	caput: string;
+	meter: string;
 	latin: string;
 	english: string;
 }
--- app/carmina.ts
+++ app/carmina.ts
@@ -4,6 +4,7 @@ export const CARMINA: Carmen[] = [
 	{ 
 		id: 1,
 		caput: 'I. ad Corneliam',
+		meter: 'none',
 		latin: `Cui dono lepidum novum libellum<br>
 						arida modo pumice expolitum?<br>
 						Corneli, tibi: namque tu solebas<br>
@@ -19,60 +20,70 @@ export const CARMINA: Carmen[] = [
 	{ 
 		id: 2,
 		caput: 'II. fletus passeris Lesbiae',
+		meter: 'none',
 		latin: `Passer, deliciae meae puellae`,
 		english: `Sparrow, my girl's delight,`
 	},
 	{ 
 		id: 3,
 		caput: 'III. fletus passeris Lesbiae',
+		meter: 'none',
 		latin: `Lugete, o Veneres Cupidinesque,`,
 		english: `Mourn, o Loves and Angels,`
 	},
 	{ 
 		id: 4,
 		caput: 'IV. de phasello',
+		meter: 'none',
 		latin: `Phaselus ille, quem videtis, hospites,`,
 		english: `Phasellus here, as you see, guests,`
 	},
 	{ 
 		id: 5,
 		caput: 'V. ad Lesbiam',
+		meter: 'none',
 		latin: `Vivamus mea Lesbia, atque amemus,`,
 		english: `Let us live, my Lesbia, and love,`
 	},
 	{ 
 		id: 6,
 		caput: 'VI. ad Flavium',
+		meter: 'none',
 		latin: `Flavi, delicias tuas Catullo,`,
 		english: `Let us live, my Lesbia, and love,`
 	},	
 	{ 
 		id: 7,
 		caput: 'V. ad Lesbiam',
+		meter: 'none',
 		latin: `Quaeris, quot mihi basiationes`,
 		english: `Let us live, my Lesbia, and love,`
 	},
 	{ 
 		id: 8,
 		caput: 'VIII. ad se ipsum',
+		meter: 'none',
 		latin: `Miser Catulle, desinas ineptire,`,
 		english: `Let us live, my Lesbia, and love,`
 	},
 	{ 
 		id: 9,
 		caput: 'IX. ad Veranium',
+		meter: 'none',
 		latin: `Verani, omnibus e meis amicis`,
 		english: `Let us live, my Lesbia, and love,`
 	},
 	{ 
 		id: 10,
 		caput: 'X. ad Varum',
+		meter: 'none',
 		latin: `Varus me meus ad suos amores`,
 		english: `Let us live, my Lesbia, and love,`
 	},
 	{ 
 		id: 11,
 		caput: 'XI. ad Furium et Aurelium',
+		meter: 'none',
 		latin: `Furi et Aureli comites Catulli,`,
 		english: `Let us live, my Lesbia, and love,`
 	},					
